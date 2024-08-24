61ff52b0b14092bd19bcef444dc15d0a5228b3b1
psj <silbaram79@naver.com>
2024-08-24 23:24:42 +0900

    배포 에러 수정

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,3 +16,18 @@ exports.createPages = async ({ actions }) => {
     defer: true,
   })
 }
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === 'build-html') {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /phaser/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    });
+  }
+};
