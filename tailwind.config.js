d5dd2beb7d8046b88d74bef35ffbee9a357a300d
psj <sj.park@qoo10.com>
2025-05-30 13:35:11 +0900

    디자인 변경

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Gatsby 소스 디렉터리 전체에 적용
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
